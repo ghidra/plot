5637b4eb44512ccf390d3304ec9a85aed985170a
ghidra <ghidra@gmail.com>
2017-12-12 00:46:31 -0500

    threading of differnt tasks, getting ready for serial connection

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tkinter import *
+import threading
 import sys
 import os
+import serial #"python -m pip install pyserial"
+#import re
 import time
 
 sys.path.append(os.getcwd()+"\\src")
@@ -13,12 +16,30 @@ import a_01_helloWorld
 
 #---------------------------------------------
 
+#canvas size
 width = 800
 height = 400
+
+#how often a new line is made in automatic draw
 afterSpeed = 100
 
 #---------------------------------------------
 
+#plotter connection
+buffer_size = 128
+baud = 9600
+serial_address = ""
+doPlot = False # this is just here if we are testing without a plotter connected, we bypass all plotter code
+
+#---------------------------------------------
+
+#this is the array of segments.
+segmentBuffer = []
+maxSegmentBufferSize = 200
+
+#---------------------------------------------
+
+#tkinter
 tk = Tk()
 tk.title( "plot" )
 canvas = Canvas(tk,width = width, height = height)
@@ -38,7 +59,6 @@ canvas.bind("<Configure>", configure)
 #-------------------------------------------------------------
 #  manual drawing
 #-------------------------------------------------------------
-
 mousePlotting = False
 mouseStartPos = []
 
@@ -63,16 +83,64 @@ canvas.bind( "<ButtonRelease-1>", mouseRelease )
 #  automatic drawing
 #------------------------------------------------------------
 
-artist = a_01_helloWorld.a_01_helloWorld( vector2(width/2.0,height/2.0) )
+class drawThread(threading.Thread):
+	def __init__( self, width, height ):
+		threading.Thread.__init__(self)
+		self.artist = a_01_helloWorld.a_01_helloWorld( vector2(width/2.0,height/2.0) )
+		self.start()
 
-def draw():
+	def run(self):
+		print( "start drawing thread" )
+		draw( self.artist )
 
-	segment = artist.update()
-	canvas.create_line(segment.p1.x,segment.p1.y,segment.p2.x,segment.p2.y,fill=segment.color)
-	tk.after(afterSpeed,draw)
+def draw( artist ):
 
-tk.after(afterSpeed,draw)
+	if len(segmentBuffer)<maxSegmentBufferSize:
+		segment = artist.update()
+		canvas.create_line(segment.p1.x,segment.p1.y,segment.p2.x,segment.p2.y,fill=segment.color)
+		segmentBuffer.append([segment.p1,segment.p2])
 
+	tk.after(afterSpeed,draw,artist)
+
+_drawThread = drawThread(width, height)
+
+#-------------------------------------------------------------
+#  plotter
+#------------------------------------------------------------
+
+class gcodeThread(threading.Thread):
+   
+	def __init__( self , serial_address, baud, buffer_size):
+		threading.Thread.__init__(self)
+		if doPlot:
+			self.device = serial.Serial(serial_address,baud)
+		self.baud = baud
+		self.buffer_size = buffer_size
+
+		## Wake up grbl
+		if doPlot:
+			self.device.write("\r\n\r\n")
+		print("init gcode thread")
+		threading.Timer(2, self.flushOnWake).start()
+
+	def flushOnWake(self):
+		if doPlot:
+			self.device.flushInput()
+		self.start()
+
+	def run(self):
+		print("start gcode thread")
+		gcode()
+
+
+def gcode():
+	print("coding it up")
+	if doPlot:
+		while len(segmentBuffer)>0:
+			segment = segmentBuffer.pop(0)
+			#print(*myData)
+
+_gcodeThread = gcodeThread(serial_address, baud, buffer_size)
 #-------------------------------------------------------------
 
 mainloop()
